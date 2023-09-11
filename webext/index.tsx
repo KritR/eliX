996987dc9748b2e50ad24e99d84dcec445534576
Winston Liu <winstonliu@microsoft.com>
2023-09-11 16:45:52 -0700

    Shenanigans

--- webext/index.tsx
+++ webext/index.tsx
@@ -1,17 +1,34 @@
-import { render } from "react-dom";
+import { render } from 'react-dom';
 import {
   FluentProvider,
   webLightTheme,
   Button
-} from "@fluentui/react-components";
+} from '@fluentui/react-components';
 
-export default function App() {
+function App({ selection }: { selection: string | undefined }) {
   return (
     <FluentProvider theme={webLightTheme}>
-      <Button appearance="primary">ELiX</Button>
+      <Button appearance="primary">{selection ?? 'Nothing'}</Button>
     </FluentProvider>
   );
 }
 
-const rootElement = document.getElementById("root");
-render(<App />, rootElement);
+async function getCurrentTab() {
+  const queryOptions = {
+    active: true,
+    lastFocusedWindow: true
+  } satisfies chrome.tabs.QueryInfo;
+  const [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
+
+const tab = await getCurrentTab();
+const selection = tab?.id ? (await chrome.scripting.executeScript({
+  func: () => window.getSelection()?.toString(),
+  target: {
+    tabId: tab.id
+  }
+}))[0].result : undefined;
+
+const rootElement = document.getElementById('root');
+render(<App selection={selection} />, rootElement);
